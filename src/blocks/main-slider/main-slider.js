b665d65c38b9125caf0e0dd8282f29c50d4309aa
des-yogi <nuevo-design@mail.ru>
2019-11-26 19:52:05 +0200

    main-slider in progress

---
+++ src/blocks/main-slider/main-slider.js
@@ -0,0 +1,36 @@
+(function(){
+
+
+  var mainSliderLeft = new Swiper ('.main-slider__left', {
+    spaceBetween: 20,
+    preloadImages: false,
+    lazy: true,
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var mainSliderRight = new Swiper ('.main-slider__right', {
+    spaceBetween: 20,
+    preloadImages: false,
+    lazy: true,
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+}());
