229802c46a302e04d81139b2097f03ace3777d5a
des-yogi <nuevo-design@mail.ru>
2019-12-02 20:10:18 +0200

    rates-slider in progress

---
+++ src/blocks/rates-slider/rates-slider.js
@@ -0,0 +1,32 @@
+(function(){
+
+
+  var ratesSlider = new Swiper ('.rates-slider__container', {
+    spaceBetween: 0,
+    slidesPerView: 1,
+    //loop: true,
+
+    breakpoints: {
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2
+      },
+      1280: {
+        slidesPerView: 3
+      },
+      1920: {
+        slidesPerView: 4
+      }
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+}());
