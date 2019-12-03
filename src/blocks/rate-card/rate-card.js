f5d69160e5af77bca796016adf722d1ba8a48551
des-yogi <nuevo-design@mail.ru>
2019-12-03 20:25:00 +0200

    rate-slider in progres

--- src/blocks/rate-card/rate-card.js
+++ src/blocks/rate-card/rate-card.js
@@ -1,4 +1,13 @@
-// document.addEventListener('DOMContentLoaded', function(){});
-// (function(){
-// код
-// }());
+$( document ).ready(function() {
+  var omegaTVinput = document.getElementById('omegaTVinput');
+  var omegaTVselect = document.getElementById('omegaTVselect');
+  var inputCheckHandler = function (e) {
+    if (omegaTVinput.checked) {
+      omegaTVselect.removeAttribute('disabled');
+    } else {
+        omegaTVselect.setAttribute('disabled', 'disabled');
+    }
+  };
+
+  omegaTVinput.addEventListener('change', inputCheckHandler);
+});
