3b35705b500c5b41d65cc55651fa87b0585ada23
des-yogi <nuevo-design@mail.ru>
2019-12-04 21:57:35 +0200

    modal request in progress

--- src/blocks/rate-card/rate-card.js
+++ src/blocks/rate-card/rate-card.js
@@ -1,13 +1,30 @@
 $( document ).ready(function() {
-  var omegaTVinput = document.getElementById('omegaTVinput');
-  var omegaTVselect = document.getElementById('omegaTVselect');
-  var inputCheckHandler = function (e) {
+
+  var rateCards = document.querySelectorAll('.rate-card');
+
+  Array.prototype.forEach.call( rateCards, function( card ) {
+    var omegaTVinput = card.querySelector('.field-toggler__input--omegaTV');
+    var omegaTVselect = card.querySelector('.field-select__select--omegaTV');
+    var selectContainer = card.querySelector('.rate-card__select--omegaTV');
+
+    var inputCheckHandler = function (e) {
     if (omegaTVinput.checked) {
-      omegaTVselect.removeAttribute('disabled');
+        omegaTVselect.removeAttribute('disabled');
+        omegaTVselect.focus();
+        selectContainer.style.borderTopColor = '#0077ff';
     } else {
         omegaTVselect.setAttribute('disabled', 'disabled');
-    }
-  };
+        selectContainer.style.borderTopColor = '#f1f1f1';
+        omegaTVselect.blur();
+      }
+    };
 
-  omegaTVinput.addEventListener('change', inputCheckHandler);
+    omegaTVinput.addEventListener('change', inputCheckHandler);
+  });
+
 });
+
+
+/*Array.prototype.forEach.call( elements, function( element ) {
+
+});*/
