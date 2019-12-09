254d2887da8d74632620627bbab7bbe8e3710b71
des-yogi <nuevo-design@mail.ru>
2019-12-09 19:44:25 +0200

    rateCalculator in progress

--- src/blocks/rate-card/rate-card.js
+++ src/blocks/rate-card/rate-card.js
@@ -7,6 +7,14 @@ $( document ).ready(function() {
     var omegaTVselect = card.querySelector('.field-select__select--omegaTV');
     var selectContainer = card.querySelector('.rate-card__select--omegaTV');
 
+    var rateTitle = card.querySelector('.rate-card__title');
+    var rateBase = card.querySelector('.base-sum');
+    var rateTotal = card.querySelector('.total-sum');
+    var connectionCost = card.querySelector('.connection-cost');
+    var prepayment = card.querySelector('.prepayment');
+    var togglers = card.querySelectorAll('.field-toggler__input');
+    var selectRateBtn = card.querySelector('.rate-card__to-submit');
+
     var inputCheckHandler = function (e) {
     if (omegaTVinput.checked) {
         omegaTVselect.removeAttribute('disabled');
@@ -19,7 +27,35 @@ $( document ).ready(function() {
       }
     };
 
+    var rateCalculator = function (e) {
+      //console.log(prepayment);
+      var totalOptions = 0;
+      var omegaTvCost = 0;
+      var totalSum;
+
+      if ( !omegaTVselect.disabled ) {
+        omegaTvCost = parseInt(omegaTVselect.value, 10);
+      }
+
+      Array.prototype.forEach.call( togglers, function( toggler ) {
+        if ( toggler.checked ) {
+          totalOptions += parseInt(toggler.value, 10);
+        }
+      });
+
+      totalSum = parseInt(rateBase.innerText, 10) + parseInt(connectionCost.innerText, 10)
+                   + parseInt(prepayment.innerText, 10) + omegaTvCost + totalOptions;
+
+      console.log(totalSum);
+      rateTotal.innerText = totalSum;
+    };
+
+    rateCalculator();
+
+
     omegaTVinput.addEventListener('change', inputCheckHandler);
+    card.addEventListener('change', rateCalculator);
+
   });
 
 });
